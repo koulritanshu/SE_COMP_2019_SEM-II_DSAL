703ae95af8b4eec359cbd74239d11edee6b9bf00
Ritanshu Koul <ritanshu.koul@gmail.com>
2023-06-08 02:08:52 +0530

    Changed TBST Code

--- Assignment_03.cpp
+++ Assignment_03.cpp
@@ -1,257 +1,264 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node{
     int data;
-    bool lflag = true, rflag = true;
-    Node *left = NULL, *right = NULL;
-
+    Node*left;
+    Node*right;
+    bool rflag;
+    bool lflag;
 public:
-    Node(int x)
-    {
+    Node(int x){
         data = x;
-    }
-    friend class tbst;
+        lflag = true;
+        rflag = true;
+        left = NULL;
+        right = NULL;
+    }    
+    friend class TBST;
 };
 
-class tbst
-{
-    Node *root;
-    Node *dummy = new Node(-999);
+class TBST{
+    Node* dummy;
+    Node* root;
 public:
-    tbst()
-    {
-        dummy->right = dummy;
+    TBST(){
+        dummy->data = -1;
         root = NULL;
+        dummy->left = root;
+        dummy->lflag = false;
+        dummy->right = dummy;
     }
-    void insert(int x)
-    {
-        Node *newNode = new Node(x);
-        if (!root)
-        {
+    void insert(int x){
+        Node* newNode = new Node(x);
+        if(root == NULL){
             root = newNode;
             root->left = dummy;
             root->right = dummy;
-            dummy->lflag = false;
-            dummy->left = root;
             return;
         }
-        Node *curr = root;
-        while (true)
-        {
-            if (curr->data < x)
-            {
-                // go to right of curr
-                if (curr->rflag == false)
-                {
-                    curr = curr->right;
-                }
-                else
-                    break;
-            }
-            else if (curr->data > x)
-            {
-                // go to left of curr
-                if (curr->lflag == false)
-                {
+        Node* curr = root;
+        while(true){
+            if(curr->data > x){
+                if(curr->lflag == false){
                     curr = curr->left;
                 }
-                else
-                    break;
+                else break;
             }
-            else
-            {
-                cout << ">> Node already present.\n";
+            else if(curr->data < x){
+                if(curr->rflag == false){
+                    curr = curr->right;
+                }
+                else break;
+            }
+            else{
+                cout << "Node is already present." << endl;
                 return;
             }
-        }
-        if (curr->data > x)
-        {
-            // add new node to left of curr
-            newNode->right = curr;
-            newNode->left = curr->left;
-            curr->lflag = false;
-            curr->left = newNode;
-        }
-        else
-        {
-            // add new node to right of curr
+        }   
+        if(curr->data < x){
+            // insert to right of curr
             newNode->left = curr;
             newNode->right = curr->right;
             curr->rflag = false;
             curr->right = newNode;
         }
+        else if(curr->data > x){
+            // insert to left of curr
+            newNode->right = curr;
+            newNode->left = curr->left;
+            curr->lflag = false;
+            curr->left = newNode;
+        }
     }
-    void inorder()
-    {
-        Node *temp = root;
-        if (!root)
-        {
-            cout << ">> Tree is empty\n";
+    void inorder(){
+        Node* curr = root;
+        if(!curr){
             return;
         }
-        while (temp != dummy)
-        {
-            while (temp->lflag == false)
-            {
-                temp = temp->left;
+        while(curr != dummy){
+            while(curr->lflag == false){
+                curr = curr->left;
             }
-            cout << temp->data << " ";
-            while (temp->rflag)
-            {
-                temp = temp->right;
-                if(temp == dummy){
+            cout << curr->data << " ";
+            while(curr->rflag){
+                curr = curr->right;
+                if(curr == dummy){
                     return;
                 }
-                cout << temp->data << " ";
+                cout << curr->data << " ";
             }
-            temp = temp->right;
+            curr = curr->right;
         }
     }
     void preorder(){
-        Node* temp = root;
-        if(!root){
-            cout << ">> Tree is empty.\n";
+        Node* curr = root;
+        if(!curr){
             return;
         }
-        while(temp!=dummy){
-            while(temp->lflag == false){
-                cout << temp->data << " ";
-                temp = temp->left;
+        while(curr != dummy){
+            while(curr->lflag == false){
+                cout << curr->data << " ";
+                curr = curr->left;
             }
-            cout << temp->data << " ";
-            while(temp->rflag){
-                temp = temp->right;
-                if(temp==dummy){
+            cout << curr->data << " ";
+            while(curr->rflag){
+                curr = curr->right;
+                if(curr == dummy){
                     return;
                 }
-            } 
-            temp = temp->right;
+            }
+            curr = curr->right;
         }
     }
     void del(int x){
         Node*curr = root;
-        Node*pre;
-        while(curr->data != x){
-            pre = curr;
-            if(curr-> data < x){
-                if(curr->rflag){
-                    cout << ">> Value not present.\n";
-                    return;
-                }
-                else curr = curr->right;
-            }
-            else if(curr->data > x){
+        Node*parent;
+        while(curr && curr->data != x){
+            parent = curr;
+            if(curr->data > x){
                 if(curr->lflag){
-                    cout << ">> Value not present.\n";
+                    cout << "Element not present in TBST." << endl;
                     return;
                 }
                 else curr = curr->left;
             }
-        }
-        if(curr->rflag == true && curr->lflag == true){
-            Node* lef = curr->left;
-            Node* rig = curr->right;
-            if(lef->right == curr){
-                lef->right = curr->right;
-                lef->rflag = true;
-                delete(curr);
-            }
-            else if(rig->left == curr){
-                rig->lflag = true;
-                rig->left = curr->left;
-                delete(curr);
+            else if(curr->data < x){
+                if(curr->rflag){
+                    cout << "Element not present in TBST." << endl;
+                    return;
+                }
+                else curr = curr->right;
             }
         }
-        else if(curr->rflag == false && curr->lflag == true){
-            // right node of curr exists
-            if(pre->left == curr){
-                pre->left = curr->right;
-                curr->right->right = pre;
+        if(curr->lflag == true && curr->rflag == true){
+            if(curr == root){
+                root = NULL;
                 delete(curr);
             }
-            else if(pre->right == curr){
-                pre->right = curr->right;
-                curr->right->left = pre;
+            else if(parent->left == curr){
+                parent->left = curr->left;
+                parent->lflag = true;
+                curr->right = NULL;
+                curr->left = NULL;
                 delete(curr);
             }
-        }
-        else if(curr->rflag == true && curr->lflag == false){
-            // left node of curr exists
-            if(pre->left == curr){
-                pre->left = curr->left;
-                curr->left->right = pre;
-                delete(curr);
-            }
-            else if(pre->right == curr){
-                pre->right = curr->left;
-                curr->left->left = pre;
+            else if(parent->right == curr){
+                parent->right = curr->right;
+                curr->left = NULL;
+                curr->right = NULL;
+                parent->rflag = true;
                 delete(curr);
             }
         }
-        else if(curr->lflag == false && curr->rflag == false){
-            // both left and right nodes of curr exist
-            int tem = 0;
+        else if(curr->rflag){
+            if(root == curr){
+                dummy->left = root->left;
+                root = dummy->left;
+                if(root->rflag){
+                    root->right = dummy;
+                }
+                if(root->lflag){
+                    root->left = dummy;
+                }
+                delete(curr);
+            }
+            else{
+                if(parent->left == curr){
+                    curr->left->right = parent;
+                    parent->left = curr->left;
+                    curr->left = curr->right = NULL;
+                    delete(curr);
+                }
+                else if(parent->right == curr){
+                    parent->right = curr->left;
+                    if(curr->left->rflag){
+                        curr->left->right = dummy;
+                    }
+                    delete(curr);
+                }
+            }
+        }
+        else if(curr->lflag){
+            if(root == curr){
+                dummy->left = curr->right;
+                root = curr->right;
+                if(root->rflag){
+                    root->right = dummy;
+                }
+                if(root->lflag){
+                    root->left = dummy;
+                }
+                delete(curr);
+            }
+            else if(parent->left == curr){
+                parent->left = curr->right;
+                if(curr->right->lflag){
+                    curr->right->left = dummy;
+                }
+                delete(curr);
+            }
+            else if(parent->right == curr){
+                curr->right->left = parent;
+                parent->right = curr->right;
+                delete(curr);
+            }
+        }
+        else if(curr->rflag == false && curr->lflag == false){
+            // find inorder successor and save that in temp variable
+            // delete inordersuccessor value
+            // replace node to delete by the value of inorder successor
             Node* temp = curr->right;
             while(temp->lflag == false){
                 temp = temp->left;
             }
-            tem = temp->data;
-            del(tem);
-            curr->data = tem;
+            int temp_value = temp->data;
+            del(temp_value);
+            curr->data = temp_value;
         }
     }
 };
 
 int main()
 {
-    tbst obj;
-    obj.insert(10);
-    obj.insert(20);
-    obj.insert(30);
-    obj.insert(40);
-    obj.insert(26);
-    obj.insert(46);
-    obj.insert(36);
-    obj.insert(37);
-    obj.insert(21);
-    obj.insert(15);
-    obj.insert(5);
-    obj.insert(3);
-    obj.insert(4);
-    obj.insert(2);
-    obj.insert(1);
-    obj.inorder();
-    cout << endl;
-    obj.preorder();
-    cout << endl;
-    cout << "-------Deleted 15-------" << endl;
-    obj.del(15);
-    obj.inorder();
-    cout << endl;
-    obj.preorder();
-    cout << endl;
-    cout << "-------Deleted 15-------" << endl;
-    cout << "-------Deleted 26-------" << endl;
-    obj.del(26);
-    obj.inorder();
-    cout << endl;
-    obj.preorder();
-    cout << endl;
-    cout << "-------Deleted 26-------" << endl;
-    cout << "-------Deleted 30-------" << endl;
-    obj.del(30);
-    obj.inorder();
-    cout << endl;
-    obj.preorder();
-    cout << endl;
-    cout << "-------Deleted 30-------" << endl;
-    cout << "-------Deleted 1-------" << endl;
-    obj.del(1);
-    obj.inorder();
-    cout << endl;
-    obj.preorder();
-    cout << endl;
-    cout << "-------Deleted 1-------" << endl;
+    TBST obj;
+    while(true){
+        cout << "1. Insert an element." << endl;
+        cout << "2. Inorder Traversal." << endl;
+        cout << "3. Preorder Traversal." << endl;
+        cout << "4. Delete an element." << endl;
+        cout << "0. Exit" << endl;
+        int ch;
+        cout << "Make choice: ";
+        cin >> ch;
+        if(ch == 1){
+            int x;
+            cout << "Enter value to insert: ";
+            cin >> x;
+            obj.insert(x);
+        }
+        else if(ch == 2){
+            cout << "----------------------------" << endl;
+            obj.inorder();
+            cout << endl;
+            cout << "----------------------------" << endl;
+        }
+        else if(ch == 3){
+            cout << "----------------------------" << endl;
+            obj.preorder();
+            cout << endl;
+            cout << "----------------------------" << endl;
+        }
+        else if(ch == 4){
+            int x;
+            cout << "Enter value you wish to delete: ";
+            cin >> x;
+            obj.del(x);
+        }
+        else if(ch == 0){
+            cout << "OK!" << endl;
+            break;
+        }
+    }
+    return 0;
 }
